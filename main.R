c108ae24e6910d77fa919903d8ac00e467db376d
Elias Santiago Cáceres Melgarejo <78571779+elias7991@users.noreply.github.com>
2022-03-23 00:25:42 -0300

    Update main.R

--- main.R
+++ main.R
@@ -13,10 +13,10 @@ library(dplyr)
 
 # *******************INFORMACION PARA AUTENTICACION*******************
 app_name <- "Analisis_Datos_Twitter"
-consumer_key <- "H2cC7eKrsCHqBoBZG6XYNeFAi"
-consumer_secret <- "z2d9Riqn3JyeOvkQ0R0E6P89Huo22SEB92GdwAbK69SsS4zt3i"
-access_token <- "1443289288817483787-Y23CyxwSuGhXX0M6yZzGKbmsvqNMQV"
-access_token_secret <- "18nVpC79FpzY46PrkAQiB9gbXCT7XTRvoL1bJQQbtXIDr"
+consumer_key <- ""
+consumer_secret <- ""
+access_token <- ""
+access_token_secret <- ""
 
 
 # *******************SECCION DE CREACION DE TOKEN DE USUARIO*******************
